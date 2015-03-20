1200850d2e8555599990a68c2f32bc4c46f52509
Alx Shcherbak <akexium@gmail.com>
2015-03-20 22:21:34 +0200

    Добавлена ЛР№1

---
+++ src/controllers/ControllerToFxml.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import functions.Lab1Func;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by Alx Shcherbak on 20.03.2015.
+ *
+ * @author Alx Shcherbak
+ */
+public class ControllerToFxml {
+    public Button doInputValue;
+    public TextField inputTextField;
+    public TextField outputTextField;
+
+    /**
+     * @param actionEvent
+     * @throws java.lang.NumberFormatException - исключение конвертации текста в цыфру
+     */
+    public void doInputValue(ActionEvent actionEvent) {
+        outputTextField.clear();
+        outputTextField.setAlignment(Pos.CENTER);
+        if (!inputTextField.getText().isEmpty()) {
+            String enterValue = new String(inputTextField.getText());
+            try {
+                double inputValueDouble = Double.parseDouble(enterValue);
+                Double resultInterpolation = new Lab1Func().doInterpolationEnterValues(inputValueDouble, outputTextField);
+                outputTextField.setText(String.valueOf(resultInterpolation));
+            } catch (NumberFormatException e) {                 // Введеное значение не цифра
+                System.out.println("Введеное начальное значение не цифра");
+                outputTextField.setText("Введеное начальное значение не цифра");
+            }
+        } else {
+            outputTextField.setText("Не введено начальное значение");
+            System.out.println("Не введено начальное значение");
+        }
+    }
+}
---
+++ src/functions/Lab1Func.java
@@ -0,0 +1,186 @@
+package functions;
+
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alx Shcherbak on 20.03.2015.
+ * @author Alx Shcherbak
+ */
+public class Lab1Func {
+    private ArrayList<Double> startParamOneArrayList = new ArrayList<Double>();
+    //    private ArrayList<Double> enterParamTwoArrayList = new ArrayList<Double>();
+    private ArrayList<Double> resultArrayList = new ArrayList<Double>();
+    TextField outputTextField;
+
+    /**
+     * Функция инициализации проведения интерполяции
+     *
+     * @param inputValue      - входная величина
+     * @param outputTextField - входное выходное текстовое поле
+     * @return
+     */
+    public Double doInterpolationEnterValues(double inputValue, TextField outputTextField) {
+        this.setEnterArrayLists();
+        Double returnValue = null;
+        if (existenceCheck(inputValue)) {
+            return getResult(inputValue);
+        }
+        this.outputTextField = outputTextField;
+        Double[] borders = borders(inputValue);
+        if (!borders[0].equals(null))
+            returnValue = doInterpolation(inputValue, borders);
+        return returnValue;
+    }
+
+    /**
+     * дебаг метод
+     *
+     * @param args
+     */
+    public static void main(String args[]) {
+        Lab1Func lab1Func = new Lab1Func();
+        //lab1Func.setEnterArrayLists();
+        System.out.println(lab1Func.doInterpolationEnterValues(1.2, null));
+        System.out.println(lab1Func);
+    }
+
+
+    /**
+     * Преобразование класса Lab1Func в строку для вывода
+     *
+     * @return -   преобразованый класс Lab1Func в строку
+     */
+    public String toString() {
+        StringBuffer returnString = new StringBuffer();
+        returnString.append("Массив входного 1 параметра M ");
+        for (Double item : this.startParamOneArrayList) {
+            returnString.append("- " + item + " ");
+        }/*
+        returnString.append("\nМассив входного 2 параметра - H ");
+        for (Double item : this.enterParamTwoArrayList){
+            returnString.append("- " + item + " ");
+        }*/
+        returnString.append("\nМассив результирующего параметра - R ");
+        for (Double item : this.resultArrayList) {
+            returnString.append("- " + item.intValue() + " ");
+        }
+        return returnString.toString();
+    }
+
+    /**
+     * Инициализация входных массивов значений
+     */
+    private void setEnterArrayLists() {
+        this.startParamOneArrayList.add((double) 0);
+        this.startParamOneArrayList.add(0.125);
+        this.startParamOneArrayList.add(0.25);
+        this.startParamOneArrayList.add(0.375);
+        this.startParamOneArrayList.add(0.5);
+        this.startParamOneArrayList.add(0.625);
+        this.startParamOneArrayList.add(0.75);
+        this.startParamOneArrayList.add(0.875);
+        this.startParamOneArrayList.add(1.0);
+        this.startParamOneArrayList.add(1.5);
+/*
+        this.enterParamTwoArrayList.add((double) 0);
+        this.enterParamTwoArrayList.add((double) 2);
+        this.enterParamTwoArrayList.add((double) 4);
+        this.enterParamTwoArrayList.add((double) 6);
+        this.enterParamTwoArrayList.add((double) 8);
+        this.enterParamTwoArrayList.add((double) 10);
+        this.enterParamTwoArrayList.add((double) 12);
+*/
+        this.resultArrayList.add((double) 9850);
+        this.resultArrayList.add((double) 8850);
+        this.resultArrayList.add((double) 8070);
+        this.resultArrayList.add((double) 7370);
+        this.resultArrayList.add((double) 6850);
+        this.resultArrayList.add((double) 6330);
+        this.resultArrayList.add((double) 5800);
+        this.resultArrayList.add((double) 5270);
+        this.resultArrayList.add((double) 4750);
+        this.resultArrayList.add((double) 4000);
+    }
+
+    /**
+     * Вывести в поле вывода сообщение
+     *
+     * @param massage - сообщение для вывода
+     */
+    private void setMassageToOutputTextField(String massage) {
+        this.outputTextField.setText(massage);
+    }
+
+    /**
+     * Определение границ для интерполяции
+     *
+     * @param inputValue - входная величина
+     * @return -   массив границ для проведения интерполяции
+     */
+    private Double[] borders(double inputValue) {
+        Double[] returnBorders = new Double[2];
+        for (int i = 0, j = 1; i < this.startParamOneArrayList.size(); i++, j++) {
+            if (((i == 0) & (this.startParamOneArrayList.get(i) > inputValue)) |
+                    ((i == this.startParamOneArrayList.size() - 1) && (this.startParamOneArrayList.get(i) < inputValue))) {
+                System.out.println("Введеное значение вне условного массива");
+                setMassageToOutputTextField("Введеное значение вне условного массива");
+                break;
+            } else if ((j < startParamOneArrayList.size()) && (this.startParamOneArrayList.get(i) < inputValue) && (this.startParamOneArrayList.get(j) > inputValue)) {
+                returnBorders = new Double[]{this.startParamOneArrayList.get(i), this.startParamOneArrayList.get(j)};
+                break;
+            }
+        }
+        return returnBorders;
+    }
+
+    /**
+     * Проверка на существование входной величины в массиве условий
+     *
+     * @param inputValue - входная величина
+     * @return -   boolean значение существования входной величины в массиве условий
+     * true    -   существует
+     * false   -   несуществует
+     */
+    private boolean existenceCheck(Double inputValue) {
+        boolean result = false;
+        for (Double item : this.startParamOneArrayList) {
+            if (item.equals(inputValue)) {
+                return true;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Получение результата функциии (с условия) по входной величине
+     *
+     * @param inputValue - входная величина
+     * @return - значение результата для водной величины
+     */
+    private Double getResult(Double inputValue) {
+        Double resultValue = null;
+        for (Double item : this.startParamOneArrayList) {
+            if (item.equals(inputValue)) {
+                resultValue = this.resultArrayList.get(startParamOneArrayList.indexOf(item));
+                break;
+            }
+        }
+        return resultValue;
+    }
+
+    /**
+     * Функция проведения интерполяции
+     *
+     * @param inputValue - входная величина
+     * @param borders    - границы интерполируемого отрезка
+     * @return -   результат интерполяции
+     */
+    private Double doInterpolation(double inputValue, Double[] borders) {
+        Double returnValue = null;
+        returnValue = getResult(borders[0]) + ((getResult(borders[1]) - getResult(borders[0])) /
+                (borders[1] - borders[0]) * (inputValue - borders[0]));
+        return returnValue;
+    }
+}

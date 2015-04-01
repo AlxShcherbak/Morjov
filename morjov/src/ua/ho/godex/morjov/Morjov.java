392a86a01a34314e0992c8126aed7d0dedf91d93
godexxx <godexxxx@gmail.com>
2015-04-01 14:39:02 +0300

    Lab2 translete

--- morjov/src/ua/ho/godex/morjov/Morjov.java
+++ morjov/src/ua/ho/godex/morjov/Morjov.java
@@ -81,7 +81,7 @@ public class Morjov extends Activity {
         tab3Log = (TextView) findViewById(R.id.tab3Log);
 
         dropdown = (Spinner) findViewById(R.id.tab3spinner1);
-        String[] items = new String[]{"Golden", "Fibonachi"};
+        String[] items = new String[]{"Золотое сечение", "Фибоначи"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
         dropdown.setAdapter(adapter);
 
